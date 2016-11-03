64f6e843b9988bbc7c156372abac322d3a2c1124
Jordan Knight <jakkaj@outlook.com>
2016-11-03 16:09:40 +1100

    Integrated Azure AD to protect the coming editing bits.
    Used https://azure.microsoft.com/en-us/documentation/samples/active-directory-dotnet-webapp-openidconnect-aspnetcore/ for the sample.

---
+++ src/DXNewsAPI/src/DXNewsAPI/Model/SetupHelpers/AuthCallbackHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DXNewsAPI.Model.SetupHelpers
+{
+    public class AuthCallbackHandler
+    {
+        public Task TokenValidated(TokenValidatedContext context)
+        {
+
+            string issuer = context.SecurityToken.Issuer;
+            string subject = context.SecurityToken.Subject;
+            string tenantID = context.Ticket.Principal.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+
+            // Build a dictionary of approved tenants
+            IEnumerable<string> approvedTenantIds = new List<string>
+            {
+                "43d9f969-d7ef-4f3f-a55b-50a48f7ae65f",
+                "9188040d-6c67-4c5b-b112-36a304b66dad" // MSA Tenant
+            };
+
+            if (!approvedTenantIds.Contains(tenantID))
+                throw new SecurityTokenValidationException();
+
+            /* ---------------------
+            // Replace this with your logic to validate the issuer/tenant
+               ---------------------       
+            // Retriever caller data from the incoming principal
+            string issuer = context.SecurityToken.Issuer;
+            string subject = context.SecurityToken.Subject;
+            string tenantID = context.Ticket.Principal.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+
+            // Build a dictionary of approved tenants
+            IEnumerable<string> approvedTenantIds = new List<string>
+            {
+                "<Your tenantID>",
+                "9188040d-6c67-4c5b-b112-36a304b66dad" // MSA Tenant
+            };
+
+            if (!approvedTenantIds.Contains(tenantID))
+                throw new SecurityTokenValidationException();
+              --------------------- */
+
+            return Task.FromResult(0);
+        }
+
+        // Handle sign-in errors differently than generic errors.
+        public Task RemoteFailure(FailureContext context)
+        {
+            context.HandleResponse();
+            context.Response.Redirect("/Home/Error?message=" + context.Failure.Message);
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/DXNewsAPI/src/DXNewsAPI/Model/SetupHelpers/AuthSetupHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DXNewsAPI.Model.SetupHelpers
+{
+    public static class AuthSetupHelper
+    {
+        public static IApplicationBuilder UseDxAuth(this IApplicationBuilder app, 
+            IServiceProvider serviceProvider, IConfigurationRoot configuration,
+            AuthCallbackHandler callbackHandler)
+        {
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                ClientId = configuration["AzureAd:ClientId"],
+                Authority = String.Format(configuration["AzureAd:AadInstance"], configuration["AzureAd:Tenant"]),
+                ResponseType = OpenIdConnectResponseType.IdToken,
+                PostLogoutRedirectUri = configuration["AzureAd:PostLogoutRedirectUri"],
+                Events = new OpenIdConnectEvents
+                {
+                    OnRemoteFailure = callbackHandler.RemoteFailure,
+                    OnTokenValidated = callbackHandler.TokenValidated
+                },
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    // Instead of using the default validation (validating against
+                    // a single issuer value, as we do in line of business apps), 
+                    // we inject our own multitenant validation logic
+                    ValidateIssuer = false,
+
+                    NameClaimType = "name"
+                }
+            });
+
+            return app;
+        }
+
+
+    }
+}

51de3f428ff9f602a1bd38d0b48dcb99afc9b572
Jordan Knight <jakkaj@outlook.com>
2016-11-08 16:51:56 +1100

    Added the media element streaming extensions.

---
+++ samples/CognitiveSampleApps/CognitiveSampleWindows/MediaElementExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CognitiveSampleWindows
+{
+    static class MediaElementExtensions
+    {
+        public static async Task PlayStreamAsync(
+          this MediaElement mediaElement,
+          IRandomAccessStream stream,
+          bool disposeStream = true)
+        {
+            // bool is irrelevant here, just using this to flag task completion.
+            TaskCompletionSource<bool> taskCompleted = new TaskCompletionSource<bool>();
+
+            // Note that the MediaElement needs to be in the UI tree for events
+            // like MediaEnded to fire.
+            RoutedEventHandler endOfPlayHandler = (s, e) =>
+            {
+                if (disposeStream)
+                {
+                    stream.Dispose();
+                }
+                taskCompleted.SetResult(true);
+            };
+            mediaElement.MediaEnded += endOfPlayHandler;
+
+            mediaElement.SetSource(stream, string.Empty);
+            mediaElement.Play();
+
+            await taskCompleted.Task;
+            mediaElement.MediaEnded -= endOfPlayHandler;
+        }
+    }
+}
---
+++ samples/CognitiveSampleApps/CognitiveSampleWindows/Model/Services/SpeechService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+
+namespace CognitiveSampleWindows.Model.Services
+{
+    class SpeechService
+    {
+        public async void DoSpeech(string text, MediaElement speechElement)
+        {
+            await _speakTextAsync("it's " + text, speechElement);
+        }
+
+        async Task _speakTextAsync(string text, MediaElement mediaElement)
+        {
+            IRandomAccessStream stream = await this._synthesizeTextToSpeechAsync(text);
+
+            await mediaElement.PlayStreamAsync(stream, true);
+        }
+
+        async Task<IRandomAccessStream> _synthesizeTextToSpeechAsync(string text)
+        {
+            // Windows.Storage.Streams.IRandomAccessStream
+            IRandomAccessStream stream = null;
+
+            // Windows.Media.SpeechSynthesis.SpeechSynthesizer
+            using (SpeechSynthesizer synthesizer = new SpeechSynthesizer())
+            {
+                // Windows.Media.SpeechSynthesis.SpeechSynthesisStream
+                stream = await synthesizer.SynthesizeTextToStreamAsync(text);
+            }
+
+            return (stream);
+        }
+    }
+}
